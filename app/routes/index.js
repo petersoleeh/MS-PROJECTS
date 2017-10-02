a934ba4c4eafe41099eb136759afabdb81875189
peter soleeh <petersoleeh@gmail.com>
2017-10-02 15:22:34 +0300

    hardcoded data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+var projects = [{
+  id: 1,
+  github repo: "https://github.com/DavidNjoroge/Kalisoft",
+  contributors: ""
+  images: ""
+}]
 
-export default Ember.Route.extend({
-});
+export default Ember.Route.extend({});
