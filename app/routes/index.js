dc08302a3f7f2779203d8b6e307577517a57862c
peter soleeh <petersoleeh@gmail.com>
2017-10-03 12:10:33 +0300

    add new key properties

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,18 +2,21 @@ import Ember from 'ember';
 var projects = [{
   id: 1,
   github: "https://github.com/DavidNjoroge/Kalisoft",
+  site: "https://davidnjoroge.github.io/Kalisoft/",
   contributors: "Njoroge",
-  images: ""
+  images: "https://screenshotmachine.com/serve.php?img=davidnjoroge-github-io-1024x768desktop-ca16b8.png"
 }, {
   id: 2,
   github: "https://github.com/Idaapayo/ECF",
+  site: "https://idaapayo.github.io/ECF/",
   contributors: "David",
-  images: ""
+  images: "https://screenshotmachine.com/serve.php?img=idaapayo-github-io-1024x768desktop-60259d.png"
 }, {
   id: 3,
   github: "https://github.com/jonnygovish/Bubble-Blaster-Game",
+  site: "https://jonnygovish.github.io/Bubble-Blaster-Game/",
   contributors: "Vinny",
-  images: ""
+  images: "https://screenshotmachine.com/serve.php?img=jonnygovish-github-io-1024x768desktop-d6420d.png"
 
 }];
 
