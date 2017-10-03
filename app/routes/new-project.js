ceb876bfeedf7d99ea2f712364a1945f88b9823e
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-10-03 16:28:24 +0300

    add static comment section

--- app/routes/new-project.js
+++ app/routes/new-project.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     submitUrl(params) {
+      console.log(params);
       var newProject = this.store.createRecord('project', params)
       newProject.save()
       this.transitionTo('index')
