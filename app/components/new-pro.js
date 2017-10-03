3204e37bf2321eb47a37b0425fc1053139b2473f
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-10-03 12:04:09 +0300

    add the api for github for retrieving the contributors and name of the project

--- app/components/new-pro.js
+++ app/components/new-pro.js
@@ -2,23 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
-    submitGithub() {
+    submitUrl() {
+
       var q = {
         github: this.get('github')
       }
       var a = q.github.split('/')
+      var aa = q.github.split('/')
       var b = a.splice(3, 2)
       var c = b.join('/')
-      // console.log(c);
+      console.log(aa);
+      var d = aa.splice(-2)
+      var e = d.join()
 
+      var array = []
+      var url = 'https://api.github.com/repos/' + c + '/contributors'
+      Ember.$.getJSON(url).then(function(response) {
+        for (var i = 0; i < response.length; i++) {
+          array.push(response[i].login)
+        }
+        // console.log(array);
+        // return array
+      })
       var params = {
+        name: e,
         github: this.get('github'),
-        userLink: c,
+        contributors: array,
         url: this.get('url')
       }
-
-      // console.log(q.github);
-      this.sendAction('submitGithub', params)
+      this.sendAction('submitUrl', params)
     }
   }
 });
--- app/routes/new-project.js
+++ app/routes/new-project.js
@@ -1,9 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions:{
-    submitGithub(params){
-      console.log(params.userLink);
+  actions: {
+    submitUrl(params) {
+      console.log(params);
     }
   }
 
