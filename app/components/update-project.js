fa76d3ef037bee8a1026dd952d5c29ac10c72a8b
peter soleeh <petersoleeh@gmail.com>
2017-10-04 11:55:15 +0300

    update the projects

---
+++ app/components/update-project.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  updateProject: false,
+  actions: {
+    updateProject() {
+      this.set('updateProject', true);
+    },
+    update(project) {
+      var params = {
+        site: this.get('site'),
+        github: this.get('github'),
+      };
+      this.set('updateProject', false);
+      this.sendAction('update', project, params);
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('project');
   },
+  actions: {
+    update(project, params) {
+      Object.keys(params).forEach(function(key) {
+        if (params[key] !== undefined) {
+          project.set(key, params[key]);
+        }
+      });
+      project.save();
+      this.transitionTo('index');
+    }
+  }
 });
