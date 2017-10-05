2f27d451dd696254c7144c4890a7c4323eedcb05
peter soleeh <petersoleeh@gmail.com>
2017-10-05 14:31:17 +0300

    ratings

---
+++ app/helpers/project-rating.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export function projectRating(params /*, hash*/ ) {
+  var project = params[0]
+
+  if (project.get('ratings').get('length') >= 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  }
+  // for (var i = 0; i < project.get('ratings').get('length'); i++) {
+  //   total = array[i] + total
+  // }
+  // return total
+
+
+}
+
+export default Ember.Helper.helper(projectRating);
