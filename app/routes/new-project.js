3eb88b2b1b8235f8206c841afc350548419cafb9
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-10-05 15:32:20 +0300

    fix some issues

--- app/routes/new-project.js
+++ app/routes/new-project.js
@@ -2,13 +2,43 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
+
     submitUrl(params) {
       // console.log(params);
       var newProject = this.store.createRecord('project', params)
       newProject.save()
       this.transitionTo('index')
-      // console.log(params);
-    }
+
+      // var contr = function(name) {
+      //   var a = name.split('/')
+      //   var b = a.splice(3, 2)
+      //   var c = b.join('/')
+      //   var array = []
+      //   var url = 'https://api.github.com/repos/' + c + '/contributors'
+      //   Ember.$.getJSON(url).then(function(response) {
+      //     var array = []
+      //     for (var i = 0; i < response.length; i++) {
+      //       array.push(response[i].login)
+      //     }
+      //     console.log(array);
+      //     var newContributor = this.store.createRecord('contributor', array)
+      //     var project = params.project
+      //     project.get('contributors').addObject(newContributor)
+      //     newContributor.save().then(function() {
+      //       return project.save()
+      //     })
+      //
+      //   })
+      // }
+      //
+      // contr(params.github)
+    },
+    // contributors(array) {
+    //   console.log(array);
+    //   var newContributors = this.store.createRecord('contributor', array)
+    // newContributors.save()
+    // },
+
   }
 
 });
