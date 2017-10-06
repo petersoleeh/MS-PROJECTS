d5bf4e0faf8a913fce58b1f7134aaeb93ba1d337
njorogedavid13@gmail.com <njorogedavid13@gmail.com>
2017-10-06 10:34:16 +0300

    fix the average rating for 0

--- app/helpers/ratings.js
+++ app/helpers/ratings.js
@@ -11,16 +11,15 @@ export function ratings(params /*, hash*/ ) {
     total = ratings[i] + total
   }
   var av = total / ratings.length
-  return av.toFixed(1)
-  // if (rentalPrice >= 150) {
-  //   return '$$$$';
-  // } else if (rentalPrice >= 100) {
-  //   return '$$$';
-  // } else if (rentalPrice >= 50) {
-  //   return '$$';
-  // } else if (rentalPrice <= 49) {
-  //   return '$';
-  // }
+  var st = av.toString()
+  console.log(st);
+  if (st === 'NaN') {
+
+    return 0
+  } else {
+    return av.toFixed(1)
+  }
+
 }
 
 
